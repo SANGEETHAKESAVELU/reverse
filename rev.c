de11835bc30201e33f53076fbd19cc2511e19be9
SANGEETHAKESAVELU <sangitweetty@gmail.com>
2016-01-29 06:15:58 -0800

    Update rev.c

---
+++ rev.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,s=0,a=0;
+    printf("enter the number");
+    scanf("%d",&n);
+    a=n;
+    while(n>0)
+    {
+        r=n%10;
+        s=s*10+r;
+        n=n/10;
+    }
+    printf(" %d\nreverse number is %d",a,s);
+    return 0;
+}
